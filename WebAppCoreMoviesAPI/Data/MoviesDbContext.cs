3c541f971e1777a6d63b2e5147ade9366f48b025
girjesh42 <kumargirjesh2442@gmail.com>
2021-03-17 16:23:17 +0530

    final

---
+++ WebAppCoreMoviesAPI/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppCoreMoviesAPI.Models;
+
+namespace WebAppCoreMoviesAPI.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppCoreMoviesAPI.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ WebAppCoreMoviesAPI/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCoreMoviesAPI.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Mid { get; set; }
+        public string Mname { get; set; }
+        public string StarCast { get; set; }
+        public string Producer { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
